7666d38555a35c3d4bbb4ce1f9153bd110cf5047
anggrii17 <anggriani22@student.univrab.ac.id>
2026-08-07 03:02:28 +0700

    fix flask predict debug

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST", "mysql.railway.internal")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_HOST = os.getenv("DB_HOST", "gondola.proxy.rlwy.net")
+DB_PORT = int(os.getenv("DB_PORT", 13719))
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "iFiXeifdjSNTvPzxCeGhPHjsexBqQwae")
 DB_NAME = os.getenv("DB_NAME", "railway")
