af64658edd03b8d8924ab94c2aae701cb2b7ee6c
anggrii17 <anggriani22@student.univrab.ac.id>
2026-08-07 01:29:09 +0700

    fix flask predict debug

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
-DB_HOST = os.environ["DB_HOST"]
-DB_PORT = int(os.environ["DB_PORT"])
-DB_USER = os.environ["DB_USER"]
-DB_PASSWORD = os.environ["DB_PASSWORD"]
-DB_NAME = os.environ["DB_NAME"]
-
-print("DB HOST =", DB_HOST)
-print("DB NAME =", DB_NAME)
+DB_HOST = os.getenv("DB_HOST", "mysql.railway.internal")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "iFiXeifdjSNTvPzxCeGhPHjsexBqQwae")
+DB_NAME = os.getenv("DB_NAME", "railway")
