24a8458e76297e5bc9254efe57b54105964946a8
anggrii17 <anggriani22@student.univrab.ac.id>
2026-08-07 00:37:36 +0700

    fix railway dependencies

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_NAME = os.getenv("DB_NAME", "smart_posture")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+print("======================")
+print("DB_HOST =", DB_HOST)
+print("DB_PORT =", DB_PORT)
+print("DB_USER =", DB_USER)
+print("DB_NAME =", DB_NAME)
+print("======================")
