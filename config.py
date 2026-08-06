e5292ea51840416bcd2c2e6e1a405b4b5c955d98
anggrii17 <anggriani22@student.univrab.ac.id>
2026-08-06 23:51:20 +0700

    initial flask deployment

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-DB_HOST = "localhost"
-DB_PORT = 3306
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "smart_posture"
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "smart_posture")
